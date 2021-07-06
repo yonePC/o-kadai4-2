886117490f896d225279fe7659786dd8130f0585
yonePC <ys.mailaddress0929@gmail.com>
2021-07-06 07:23:50 +0000

    応用課題4

---
+++ bookers2/app/controllers/users_controller.rb
@@ -0,0 +1,54 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :ensure_correct_user, only: [:edit, :update]
+
+  def show
+    @user = User.find(params[:id])
+    @books = @user.books
+    @book = Book.new
+  end
+
+  def index
+    @users = User.all
+    @book = Book.new
+    
+   
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to user_path(@user), notice: "You have updated user successfully."
+    else
+      render "edit"
+    end
+  end
+  
+  def followings
+     @user = User.find(params[:id])
+    # @followings = @user.following_users
+  end
+  
+  def followers
+     @user = User.find(params[:id])
+    # @followers = @user.follower_users
+  end
+  
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
+  end
+
+  def ensure_correct_user
+    @user = User.find(params[:id])
+    unless @user == current_user
+      redirect_to user_path(current_user)
+    end
+  end
+  
+  
+end
